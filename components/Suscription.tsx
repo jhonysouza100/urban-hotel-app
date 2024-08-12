caca72284bc040a08d7186ae3e2fcbfe6237331d
Jhony Souza <jhonatansouzameza100@gmail.com>
2024-08-12 06:34:09 -0300

    120820240634

--- components/Suscription.tsx
+++ components/Suscription.tsx
@@ -10,20 +10,20 @@ export default function Suscription() {
     <section className="join section bg-container">
       <div className="join-container section-container">
         <div className="join-content grid gap-6 gap-y-12 pb-6 justify-center sm:grid-cols-2 md:grid-cols-a sm:items-center xl:pt-4 xl:pb-20 lg:gap-x-32 lg:grid-cols-b">
-          <div className="join-data text-center aspect-square self-start">
-            <h2 className="section-title sm:text-start">{TEXT.joinTitle1} <br /> {TEXT.joinTitle2}</h2>
-            <p className="join-description sm:text-start mb-10 xl:mb-12">{TEXT.joinText1}</p>
+          <div className="join-data text-center flex flex-col justify-between aspect-square">
+            <h2 className="section-title mb-0 sm:text-start">{TEXT.joinTitle1} <br /> {TEXT.joinTitle2}</h2>
+            <p className="join-description sm:text-start">{TEXT.joinText1}</p>
             <SuscriptionForm />
           </div>
 
-          <div className="join-image sm:-order-1 block w-full h-auto relative overflow-hidden aspect-[5/4]]">
+          <div className="join-image -order-1 block w-full h-auto relative overflow-hidden aspect-square">
             <Image priority
              className="join-img transition-transform duration-500 w-full h-full block top-0 left-0 object-cover" alt="Imagen de la habitación"
              src="/img/join-image.jpeg" 
              width={320} height={720} 
              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
-            <div className="shading"></div>
+            {/* <div className="shading"></div> */}
           </div>
         </div>
       </div>
