9c31c35023b5c4abca617bbebc5cb95de66fb040
Jhony Souza <100396330+jhonysouza100@users.noreply.github.com>
2024-09-04 08:24:05 -0300

    Add relative container to testimonilas title

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -10,26 +10,37 @@ export default async function Testimonials() {
   const REVIEWS: Review[] = await getAllReviews();
 
   return (
-    <section className="testimonials section container max-w-full px-2 sm:px-0" id="testimonials">
-      <div className="section-data container section-container grid grid-cols-1 md:grid-cols-2">
-        <div className="box text-center p-6">
-          <span className="flex items-center justify-start text-xl gap-2 font-montserrat">
-            <Rating className="!text-secondary" name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
-            <span className="text-muted-foreground"> 4.4 Rating</span>
-          </span>
+    <section className="testimonials section" id="testimonials">
+      
+      <div className="absolute w-full bg-container left-0 -bottom-16 pb-16 border-b border-primary">
+        
+        <div className="section-data container section-container grid grid-cols-1 md:grid-cols-2">
           
-          <h3 className="section-title font-montserrat text-start">{TEXT.testimonilasTitle1}<br/>{TEXT.testimonilasTitle2}</h3>
-          <p className="text-base text-muted-foreground text-start mb-5">{TEXT.testimonilasDescription1}</p>
-          <div className="box text-start space-y-2">
-            <span className="text-muted-foreground inline-flex gap-4">
-              <Avatar className='!w-6 !h-6' src="https://www.gstatic.com/images/branding/product/1x/googleg_48dp.png" alt="google logo" />
-              <Avatar className='!w-6 !h-6' src="https://www.gstatic.com/travel-hotels/branding/icon_100532569.png" alt="google logo" />
+          <div className="box text-center p-6">
+            {/*Tooltip */}
+            <span className="flex items-center justify-start sm:justify-center text-xl gap-2 font-montserrat">
+              <Rating className="!text-secondary" name="half-rating-read" defaultValue={4.5} precision={0.5} readOnly />
+              <span className="text-muted-foreground"> 4.4 Rating</span>
             </span>
+            {/*Title */}
+            <h3 className="section-title font-montserrat text-start sm:text-center">{TEXT.testimonilasTitle1}<br/>{TEXT.testimonilasTitle2}</h3>
+            <p className="text-base text-muted-foreground text-start sm:text-center mb-5">{TEXT.testimonilasDescription1}</p>
+            <div className="box text-start sm:text-center space-y-2">
+              <span className="text-muted-foreground inline-flex gap-4">
+                <Avatar className='!w-6 !h-6' src="https://www.gstatic.com/images/branding/product/1x/googleg_48dp.png" alt="google logo" />
+                <Avatar className='!w-6 !h-6' src="https://www.gstatic.com/travel-hotels/branding/icon_100532569.png" alt="google logo" />
+              </span>
+            </div>
           </div>
+          
+          <div className="box"></div>
+          
         </div>
       </div>
 
-      <TestimonialsSwiper reviews={REVIEWS} />
+      <div className="container max-w-full px-2 sm:px-0">
+        <TestimonialsSwiper reviews={REVIEWS} />
+      </div>
     </section>
   );
 }
