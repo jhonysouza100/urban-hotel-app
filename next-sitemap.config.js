6c1fbcb1f5427c6f8d87b7f0d78e27ac7ba6cd04
any <loquito@gmail.com>
2024-12-08 05:41:16 -0300

    081220240540-acualizacion de sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,45 @@
 /** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
   siteUrl: 'https://www.iguazuurbanhotel.com',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap-index.xml'], // <= exclude here
+  exclude: ['/login', '/verify', '/register'],
+  alternateRefs: [
+    {
+      href: 'https://www.iguazuurbanhotel.com/pt',
+      hreflang: 'pt',
+    },
+    {
+      href: 'https://www.iguazuurbanhotel.com/en',
+      hreflang: 'en',
+    },
+  ],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+  ],
   robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://www.iguazuurbanhotel.com/server-sitemap-index.xml', // <= add here
-    ]
-  }
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: ['/login', '/verify', '/register'],
+      },
+    ],
+  },
 }
