38b00f11b9339c9c8d783847f6188594cb9738d0
any <loquito@gmail.com>
2025-03-20 06:16:02 -0300

    200320250615

--- src/static/html_content.js
+++ src/static/html_content.js
@@ -1,145 +1,77 @@
-const htmlContent = 
+const htmlContent =
 `
-  <!DOCTYPE html>
-  <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Hotel Iguazú Urban Express</title>
-      <style>
-        body {
-          font-family: Arial, sans-serif;
-          background-color: #f4f4f4;
-          color: #333;
-          margin: 0;
-          padding: 0;
-        }
-        .container {
-          width: 100%;
-          max-width: 600px;
-          margin: 20px auto;
-          background-color: #ffffff;
-          padding: 20px;
-          border-radius: 8px;
-          box-shadow: 0 4px 6px rgba(0,0,0,0.1);
-        }
-        .header {
-          text-align: center;
-        }
-        .header h1 {
-          color: #0067b8;
-        }
-        .content {
-          margin-top: 20px;
-        }
-        .footer {
-          text-align: center;
-          margin-top: 30px;
-          font-size: 12px;
-          color: #888;
-        }
-        img {
-          width: 80%;
-          max-height: 340px;
-          object-fit: cover;
-          border-radius: 8px;
-          margin: 0 auto 20px auto;
-          display: block;
-        }
-        .content p {
-          font-size: 16px;
-          line-height: 1.6;
-        }
-        .content h2 {
-          color: #0067b8;
-          margin-top: 25px;
-        }
-        .amenities-list {
-          list-style-type: none;
-          padding-left: 0;
-        }
-        .amenities-list li {
-          margin-bottom: 10px;
-          padding-left: 30px;
-          position: relative;
-        }
-        .amenities-list li span {
-          position: absolute;
-          left: 0;
-        }
-        .schedule-list {
-          list-style-type: none;
-          padding-left: 0;
-        }
-        .schedule-list li {
-          margin-bottom: 10px;
-          padding-left: 30px;
-          position: relative;
-        }
-        .schedule-list li span {
-          position: absolute;
-          left: 0;
-        }
-        .payment-list {
-          list-style-type: none;
-          padding-left: 0;
-        }
-        .payment-list li {
-          margin-bottom: 10px;
-          padding-left: 30px;
-          position: relative;
-        }
-        .payment-list li span {
-          position: absolute;
-          left: 0;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="container">
-        <div class="header">
-          <h1>Lo esperamos en la ciudad de las Cataratas</h1>
-        </div>
-        <div class="content">
-          <img src="https://www.iguazuurbanhotel.com/_next/image?url=%2Fimg%2Fgallery-img-8.jpg&w=640&q=100" alt="Vista de las Cataratas del Iguazú">
-          
-          <h2>Nuestras Habitaciones</h2>
-          <p>🛎 El Hotel Iguazú Urban Express, cuenta con habitaciones confortables singles, dobles y triples, (puede ser con cama matrimonial y una soltero o 3 solteros) aire acondicionado, TV por cable y baño privado, ropa de cama y toallas.</p>
-          <p>🅿️ Estacionamiento sobre la calle, contamos con cámaras de seguridad.</p>
-          
-          <h2>El valor incluye:</h2>
-          <ul class="amenities-list">
-            <li><span>🔸</span> El uso de la piscina 🏊‍♀‍</li>
-            <li><span>🔸</span> Limpieza de la habitación</li>
-            <li><span>🔸</span> Wi-Fi en todo el predio</li>
-            <li><span>🔸</span> Desayuno</li>
+<html>
+  <body style="margin:0; padding:0; font-family: Arial, sans-serif;">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="720" style="max-width:720px; background-color:#ffffff; border-top-left-radius: 24px; border-top-right-radius: 24px; border-bottom-right-radius:8px; border-bottom-left-radius:8px; box-shadow:0 4px 6px rgba(0,0,0,0.1); margin:20px auto;">
+      <tr>
+        <td style="padding:0;">          
+          <!-- Profile content -->
+          <div style="display: flex; align-items: center; padding: 16px; background-color: #0067b8; border-top-left-radius: 8px; border-top-right-radius: 8px;">
+            <!-- Profile avatar -->
+            <div style="margin-right: 12px;">
+              <div style="overflow: hidden; object-fit: cover; width: 48px; height: 48px; border-radius: 50%; background-color: #9C27B0; display: flex; align-items: center; justify-content: center; border: 1px solid hsl(34, 44%, 65%);">
+                <img style="width: 48px; height: 48px;" src="https://www.iguazuurbanhotel.com/android-chrome-192x192.png" alt="Profile image" />
+              </div>
+            </div>
+            
+            <!-- Profile info -->
+            <div style="flex: 1;">
+              <h2 style="font-size: 18px; font-weight: 600; color: #fff; margin: 0;">Reservas</h2>
+              <p style="font-size: 14px; color: #eee; margin: 0;">@iguazuurbanhotel</p>
+            </div>
+            
+            <!-- More options button
+             -->
+            <a href="https://www.iguazuurbanhotel.com" target="_blank" rel="noopener noreferrer" style="width: 24px; height: 24px; border-radius: 50%; display: flex; align-items: center; justify-content: center;">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="6" r="2" fill="#eee"/>
+                <circle cx="12" cy="12" r="2" fill="#eee"/>
+                <circle cx="12" cy="18" r="2" fill="#eee"/>
+              </svg>
+            </a>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td style="padding:20px;">
+          <img src="https://www.iguazuurbanhotel.com/_next/image?url=%2Fimg%2Fgallery-img-8.jpg&w=640&q=100" alt="Vista de las Cataratas del Iguazú" style="width:100%; max-width:520px; display:block; margin:0 auto 20px auto; border-radius:8px;">
+          <h2 style="color:#0067b8; margin-top:0;">Nuestras Habitaciones</h2>
+          <p style="font-size:16px; line-height:1.6; color:#333; margin:0 0 15px 0;">🛎 El Hotel Iguazú Urban Express, cuenta con habitaciones confortables singles, dobles y triples, (puede ser con cama matrimonial y una soltero o 3 solteros) aire acondicionado, TV por cable y baño privado, ropa de cama y toallas.</p>
+          <p style="font-size:16px; line-height:1.6; color:#333; margin:0 0 15px 0;">🅿️ Estacionamiento sobre la calle, contamos con cámaras de seguridad.</p>
+          <h2 style="color:#0067b8; margin:0 0 10px 0;">El valor incluye:</h2>
+          <ul style="list-style:none; padding:0; margin:0 0 15px 0;">
+            <li style="font-size:16px; margin-bottom:10px;">🔸 El uso de la piscina 🏊‍♀‍</li>
+            <li style="font-size:16px; margin-bottom:10px;">🔸 Limpieza de la habitación</li>
+            <li style="font-size:16px; margin-bottom:10px;">🔸 Wi-Fi en todo el predio</li>
+            <li style="font-size:16px; margin-bottom:10px;">🔸 Desayuno</li>
           </ul>
-          
-          <h2>Horarios</h2>
-          <ul class="schedule-list">
-            <li><span>📌⏲️</span> Contamos con personal las 24 hs. que pueden recibirlos.</li>
-            <li><span>📌⏲️</span> Horario de ingreso a partir de las 14:00 hs.</li>
-            <li><span>📌⏲️</span> Horario de salida hasta las 10:00 am</li>
-            <li><span>📌⏲️</span> Horario de desayuno de 07:00 a 10:00 am</li>
-            <li><span>📌⏲️</span> Horario de piscina de 12:00 pm a 21:00 pm.</li>
+          <h2 style="color:#0067b8; margin:0 0 10px 0;">Horarios</h2>
+          <ul style="list-style:none; padding:0; margin:0 0 15px 0;">
+            <li style="font-size:16px; margin-bottom:10px;">📌⏲️ Contamos con personal las 24 hs. que pueden recibirlos.</li>
+            <li style="font-size:16px; margin-bottom:10px;">📌⏲️ Horario de ingreso a partir de las 14:00 hs.</li>
+            <li style="font-size:16px; margin-bottom:10px;">📌⏲️ Horario de salida hasta las 10:00 am.</li>
+            <li style="font-size:16px; margin-bottom:10px;">📌⏲️ Horario de desayuno de 07:00 a 10:00 am.</li>
+            <li style="font-size:16px; margin-bottom:10px;">📌⏲️ Horario de piscina de 12:00 pm a 21:00 pm.</li>
           </ul>
-          
-          <h2>Forma de pago</h2>
-          <p>🔴 Aceptamos:</p>
-          <ul class="payment-list">
-            <li><span>✅</span> Efectivo</li>
-            <li><span>✅</span> Transferencia</li>
-            <li><span>✅</span> Tarjeta débito</li>
-            <li><span>✅</span> Tarjeta crédito, en un pago.</li>
+          <h2 style="color:#0067b8; margin:0 0 10px 0;">Forma de pago</h2>
+          <p style="font-size:16px; line-height:1.6; color:#333; margin:0 0 15px 0;">🔴 Aceptamos:</p>
+          <ul style="list-style:none; padding:0; margin:0 0 15px 0;">
+            <li style="font-size:16px; margin-bottom:10px;">✅ Efectivo</li>
+            <li style="font-size:16px; margin-bottom:10px;">✅ Transferencia</li>
+            <li style="font-size:16px; margin-bottom:10px;">✅ Tarjeta débito</li>
+            <li style="font-size:16px; margin-bottom:10px;">✅ Tarjeta crédito, en un pago.</li>
           </ul>
-          <p>Para tomar la reserva le solicitamos el pago de la primer noche por transferencia, el resto lo abona al ingreso.</p>
-        </div>
-        <div class="footer">
-          <p>&copy; 2024 Iguazú Urban Hotel | Todos los derechos reservados</p>
-        </div>
-      </div>
-    </body>
-  </html>
+          <p style="font-size:16px; line-height:1.6; color:#333; margin:0;">Para tomar la reserva le solicitamos el pago de la primer noche por transferencia, el resto lo abona al ingreso.</p>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" style="padding:20px; font-size:12px; color:#888;">
+          &copy; 2025 Iguazú Urban Hotel | Todos los derechos reservados
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
 `;
 
 export default htmlContent;
